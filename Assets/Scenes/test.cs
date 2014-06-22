98b808638af9e524309828f3959f2684a07b88b7
Nguyen Chi Thien <nc.thien@gloops.com>
2014-06-22 17:26:34 +0700

    - Test path following with iTween

---
+++ Assets/Scenes/test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("test path"), "time", 10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
