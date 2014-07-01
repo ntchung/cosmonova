e08e0b3b05cde81a3c4e5b42ccfa03d9db85efe2
Nguyen Chi Thien <nc.thien@gloops.com>
2014-07-01 22:33:39 +0700

    + Add basic ship movement

---
+++ Assets/Scripts/Thruster.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thruster : MonoBehaviour 
+{
+	private bool isActive = false;	
+
+	private ParticleSystem cacheParticleSystem;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cacheParticleSystem = particleSystem;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isActive) 
+		{
+			if (cacheParticleSystem != null) 
+			{	
+				cacheParticleSystem.enableEmission = true;
+			}		
+		} 
+		else 
+		{
+			if (cacheParticleSystem != null) 
+			{				
+				cacheParticleSystem.enableEmission = false;				
+			}
+		}
+	}
+
+	public void StartThruster() 
+	{
+		isActive = true; 
+	}
+	
+	public void StopThruster() 
+	{
+		isActive = false; 
+	}
+}
